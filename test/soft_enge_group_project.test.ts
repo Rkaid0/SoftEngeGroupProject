e83c17abcd9e729141d075ad3e1f6107f1b8b7b8
Artyom Yesayan <yesart8@gmail.com>
2025-11-17 16:20:26 -0500

    Created CDK project

---
+++ test/soft_enge_group_project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SoftEngeGroupProject from '../lib/soft_enge_group_project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/soft_enge_group_project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SoftEngeGroupProject.SoftEngeGroupProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
